6a4ca00217fde286e10637c8c93b51c4a438d7c6
Carlos Aldazosa <carlos.aldazosa@outlook.com>
2022-05-10 09:18:23 -0400

    Web design and API hook conection

---
+++ src/services/api/index.js
@@ -0,0 +1,32 @@
+const API = process.env.NEXT_PUBLIC_API_URL;
+const VERSION = process.env.NEXT_PUBLIC_API_VERSION;
+
+const endPoints = {
+  products:{
+    getProducts: (limit, offset) => `${API}/api/${VERSION}/products?limit=${limit}&offset=${offset}`,
+    addProducts: `${API}/api/${VERSION}/products/`,
+    getProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
+    updateProducts: (id) => `${API}/api/${VERSION}/products/${id}`,
+    deleteProducts: (id) => `${API}/api/${VERSION}/products/${id}`
+  },
+  users:{
+    getUsers: `${API}/api/${VERSION}/users`,
+    postUsers: `${API}/api/${VERSION}/users`,
+  },
+  auth: {
+    login: `${API}/api/${VERSION}/auth/login`,
+    profile: `${API}/api/${VERSION}/auth/profile`
+  },
+  categories:{
+    getCategoriesList: `${API}/api/${VERSION}/categories`,
+    addCategory: `${API}/api/${VERSION}/categories`,
+    getCategoryItem: (id) => `${API}/api/${VERSION}/categories/${id}/products`,
+    updateCategories: (id) => `${API}/api/${VERSION}/categories/${id}`,
+  },
+  files:{
+    addImage: `${API}/api/${VERSION}/files/upload`,
+    getFiles: (fileName) => `${API}/api/${VERSION}/${fileName}`
+  }
+}
+
+export default endPoints;
